43e0ddb5228c76004c1a6bd402c21a2fd6daf221
JESUS <sakrateesh007@gmail.com>
2022-07-11 13:18:04 +0530

    Third commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-import taskRouter from "../Backend/routes/taskRouter.js";
+import taskRouter from "./routes/taskRouter.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
